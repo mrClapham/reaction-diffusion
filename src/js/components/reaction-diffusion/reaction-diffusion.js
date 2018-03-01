96adca9d3ecde604f30b0107fa436ff33fdca7db
Graham Clapham <grahamclapham@grahamsegreymbp.home>
2018-03-01 23:18:32 +0000

    First run at the formula.

--- src/js/components/reaction-diffusion/reaction-diffusion.js
+++ src/js/components/reaction-diffusion/reaction-diffusion.js
@@ -4,7 +4,11 @@ const defaults = {
     grid : [],
     next : [],
     width : 800,
-    height : 200
+    height : 200,
+    dA:1,
+    dB:0.5,
+    feed:0.55,
+    k:0.061
 }
 
 let grid = [];
@@ -21,15 +25,31 @@ const makeGrid = (w,h)=>{
     return grid;
 };
 
+const swap = ()=>{
+    let temp = grid;
+    grid = next;
+    next = grid;
+};
+
+const laplaceA = ()=> {
+    1
+}
+const laplaceB = ()=> {
+    1
+}
+
 export default (targDiv, config)=> {
     const props = {...defaults, ...config};
     grid = makeGrid(props.width, props.height)
-    //next = makeGrid(props.width, props.height)
+    next = makeGrid(props.width, props.height)
+
+
 
     const BaseClass = (sketch)=>{
         sketch.setup = ()=>{
             sketch.createCanvas(props.width, props.height);
             sketch.pixelDensity(1);
+            sketch.frameRate(2)
         };
         sketch.draw = ()=>{
             sketch.loadPixels() 
@@ -37,8 +57,27 @@ export default (targDiv, config)=> {
 
             for(let x=0; x<props.width; x++){
                 for(let y=0; y<props.height; y++){
+                    let a = grid[x][y].a
+                    let b = grid[x][y].b
+                    next[x][y].a =  a + 
+                                    ( props.dA + laplaceA() * a ) -
+                                    ( a * b * b ) + 
+                                    ( props.feed * (1 - a) );
+
+                    next[x][y].b =  b + 
+                                    ( props.dB + laplaceB() * b ) -
+                                    ( a * b * b ) - 
+                                    ( props.k + props.feed ) *  b ;
+
+
+                    next[x][y].b = grid[x][y].b * 0.9;
+                }
+            }
+
+            for(let x=0; x<props.width; x++){
+                for(let y=0; y<props.height; y++){
                     let pix = (x + y * props.width) * 4;
-                    sketch.pixels[pix+0] = grid[x][y].a;
+                    sketch.pixels[pix+0] = next[x][y].a;
                     sketch.pixels[pix+1] = 0;
                     sketch.pixels[pix+2] = grid[x][y].b;
                     sketch.pixels[pix+3] = 255;
@@ -47,7 +86,7 @@ export default (targDiv, config)=> {
 
             sketch.updatePixels()
             sketch.ellipse(50, 50, 80, 80);
-
+            swap()
         };
     };
 
